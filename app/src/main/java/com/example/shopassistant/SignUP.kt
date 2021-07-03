db1388fbae7a8a197ea456ab65d4e81205e9fb85
Matimateokol <58779750+Matimateokol@users.noreply.github.com>
2021-07-03 21:54:21 +0200

    Add SignIn and SignUp activities

---
+++ app/src/main/java/com/example/shopassistant/SignUP.kt
@@ -0,0 +1,11 @@
+package com.example.shopassistant
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUP : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
